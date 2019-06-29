5e59869acf6bf079a9013795a71e327c3bfd11f3
van le <vanlecs09@gmail.com>
2019-06-29 20:00:01 +0700

    ~ downgrade unity version 2019.1

--- RPG2D/Assets/Game/Scripts/Skills/FreezeSystem.cs
+++ RPG2D/Assets/Game/Scripts/Skills/FreezeSystem.cs
@@ -9,16 +9,10 @@ public class FreezeSystem : ReactiveSystem
 
     protected void Process(List<Entity> entities)
     {
-        
         foreach (var skillEntity in entities)
         {
-            UnityEngine.Debug.Log("frezze react "  + skillEntity.creationIndex);
             var freeze = skillEntity.Get<FreezeComponent>();
             var targets = skillEntity.GetComponent<TargetsComponent>().listEntityTarget;
-            if(freeze == null){
-                UnityEngine.Debug.LogWarning("freeze component is null " + skillEntity.creationIndex);
-            }
-            // var targetFreezeEntities = freeze.targetEntities;
             var freezeTime = freeze.timeFreeze;
             foreach (var targetEntity in targets)
             {
